9f49bd50a3165ead00319d9dfba51edf8b44403e
Vladyslav Zakharov <vladyslavzakharovdk@gmail.com>
2024-06-03 16:40:17 +0300

    feat: render

--- src/app/complex-dashboard/@notifications/page.tsx
+++ src/app/complex-dashboard/@notifications/page.tsx
@@ -1,5 +1,12 @@
 import { Card } from "@/components/Card";
+import Link from "next/link";
 
-export default function Notification () {
-  return <Card>Notification</Card>
+export default function Notification() {
+  return (
+    <Card>
+      <h1>Notification</h1>
+
+      <Link href="/complex-dashboard/archived">Archived</Link>
+    </Card>
+  )
 }
